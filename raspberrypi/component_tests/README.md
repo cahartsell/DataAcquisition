839cb2cf256e43ce4a7101f685e050ee33b66795
Charlie Hartsell <HartsellME@gmail.com>
2018-07-15 15:44:40 -0500

    Added raspberry pi GPIO test

--- raspberrypi/component_tests/README.md
+++ raspberrypi/component_tests/README.md
@@ -1,7 +1,2 @@
-Component tests are still in .ino format for simplicity.
-PlatformIO is picky about configuring multiple, seperate programs as build targets.
-
-Just compile and run these tests with the standard Arduino IDE.
-Note that some tests import libraries.
-With the standard Arduino IDE, these libraries must either be in the same directory, or they must be placed into the Arduino sketchbook location.
-The default location for Arduino sketchbook on Windows is "C:\<User_documents_folder>\Arduino\libraries".
+Component tests copied over from Arduino code.
+Must be converted to a standard C/C++ file using R-PI libraries before running on raspberry pi.
---
+++ raspberrypi/component_tests/raspberrypi_GPIO/gpio_test.c
@@ -0,0 +1,56 @@
+//
+// Created by Charlie on 7/15/2018.
+// Basic test of Raspberry PI GPIO using "WiringPi" library
+//
+
+#include <wiringPi.h>
+#include <stdio.h>
+#include <time.h>
+#include <mem.h>
+#include <stdlib.h>
+
+#define LOOP_COUNT 100
+#define WRITE_STABILIZE_DELAY_MICRO_S 1000
+#define WRITE_PIN 0
+#define READ_PIN 1
+
+int main (void)
+{
+    int i, wrote[LOOP_COUNT], read[LOOP_COUNT], rv, nextWrite, nextRead;
+    struct timespec tv;
+
+    // Zero arrays
+    memset(wrote, 0, sizeof(int) * LOOP_COUNT);
+    memset(read, 0, sizeof(int) * LOOP_COUNT);
+
+    // Init wiringPi and GPIO pins
+    wiringPiSetup();
+    pinMode(WRITE_PIN, OUTPUT);
+    pinMode(READ_PIN, INPUT);
+
+    // Seed RNG
+    srand(time(0));
+
+    // Fill timespec
+    // This assumes stabilize delay is always less than 1 second
+    tv.tv_nsec = WRITE_STABILIZE_DELAY_MICRO_S * 1000;
+    tv.tv_sec = 0;
+
+    // Write random value to one pin, wait, then read the value at another pin.
+    for(i = 0; i < LOOP_COUNT; i++) {
+        nextWrite = rand() % 2;
+        digitalWrite(WRITE_PIN, nextWrite);
+        nanosleep(&tv, NULL);
+        nextRead = digitalRead(READ_PIN);
+        wrote[i] = nextWrite;
+        read[i] = nextRead;
+    }
+
+    // Print results
+    printf("Wrote | Read");
+    for(i = 0; i < LOOP_COUNT; i++) {
+        printf("%d\t%d", wrote[i], read[i]);
+    }
+
+    return 0;
+}
