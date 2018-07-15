cd695423de193ae7a9dfacdff3092bed5d8424b2
Charlie Hartsell <HartsellME@gmail.com>
2018-07-15 15:43:49 -0500

    Bug fixes/improvement to Vagrant provisioning

--- DevVM/startup.sh
+++ DevVM/startup.sh
@@ -1,20 +1,29 @@
 # Install some prereqs
 apt-get update
-apt-get install git rsync cmake lib32z1 g++ lib32stdc++6
+apt-get -y install git cmake lib32z1 g++ lib32stdc++6
 
 # Set password for user "vagrant" to "vagrant"
 echo -e "vagrant\nvagrant\n" | passwd vagrant
 
 # Make folders and clone data aquisition and raspberrypi tools repos
-mkdir /home/vagrant/data-aquisition/
-mkdir /home/vagrant/raspberrypi/
-git clone https://github.com/cahartsell/DataAquisition.git /home/vagrant/data-aquisition
-git clone git://github.com/raspberrypi/tools.git /home/vagrant/raspberrypi/tools
-chown -R vagrant:vagrant /home/vagrant/data-aquisition/
-chown -R vagrant:vagrant /home/vagrant/raspberrypi/
+if [ ! -d "/home/vagrant/data-aquisition/" ]; then
+	mkdir /home/vagrant/data-aquisition/
+	git clone https://github.com/cahartsell/DataAquisition.git /home/vagrant/data-aquisition
+	chown -R vagrant:vagrant /home/vagrant/data-aquisition/
+fi
+
+if [ ! -d "/home/vagrant/raspberrypi/" ]; then
+	mkdir /home/vagrant/raspberrypi/
+	git clone git://github.com/raspberrypi/tools.git /home/vagrant/raspberrypi/tools
+	chown -R vagrant:vagrant /home/vagrant/raspberrypi/
+fi
 
 # Add cross compiler to PATH
-echo "PATH=$PATH:/home/vagrant/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin" >> /home/vagrant/.bashrc
+if [[ ":$PATH:" == *":/home/vagrant/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin:"* ]]; then
+  echo "Your path is correctly set"
+else
+  echo "PATH=$PATH:/home/vagrant/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin" >> /home/vagrant/.bashrc
+fi
 
 # CMAKE config for cross compilation
 echo "SET(CMAKE_SYSTEM_NAME Linux)
